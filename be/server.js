fc7f17c2a0b870fefd8b1ef791e8bd37de52d38d
André Ferreira <andreferreiramusic@gmail.com>
2024-10-30 15:18:49 +0100

    mongoose_database

--- be/server.js
+++ be/server.js
@@ -1,17 +1,29 @@
-import express from 'express'
-import { config } from 'dotenv'
-const port = process.env.PORT || 3001
+import express from "express";
+import { config } from "dotenv";
+import mongoose from "mongoose";
 
+config();
+
+const port = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
+const initServer = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URL}`);
+    console.log("Connected to Articoli DB");
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`
-    )
-});
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error, "No connection to Articoli DB");
+  }
+};
+
+initServer();
